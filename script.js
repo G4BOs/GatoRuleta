8132db8ef65c16a16ea26891ffab3941580eb6d6
G4BOs <108361351+G4BOs@users.noreply.github.com>
2023-12-29 17:21:20 -0300

    V1

---
+++ script.js
@@ -0,0 +1,40 @@
+var items = ['😺','😸','😹','😻','😼','😽','🙀','😿','😾']
+const PLAY= document.getElementById('play');
+const ojo1=document.getElementById('ojo1');
+const ojo2=document.getElementById('ojo2');
+const BOCA = document.getElementById('boca');
+const LENGUA = document.getElementById('lengua');
+const GANO = document.getElementById('ganaste');
+const GATO = document.getElementById('gato');
+const VIDEO = document.getElementById('gatobailando');
+
+
+
+PLAY.addEventListener('click',play);
+
+
+
+function play(){
+    let irandom1 = items[Math.floor(Math.random() * items.length)];
+let irandom2 = items[Math.floor(Math.random() * items.length)];
+
+    
+
+    ojo1.innerHTML=irandom1;
+    ojo2.innerHTML=irandom2;
+
+    console.log(irandom1)
+    console.log(irandom2)    
+
+    if (irandom1==irandom2){
+        GANO.style.display='block';
+        LENGUA.style.display='block';
+        ; PLAY.disabled=true;
+        GATO.style.scale='0.6';
+        GATO.style.translate='0 -300px';
+        VIDEO.play();
+        VIDEO.style.display='block';
+    }
+
+    
+}
