3303a361544546a8ffd1ac7f05a6bc324c1c052c
G4BOs <108361351+G4BOs@users.noreply.github.com>
2023-12-29 23:57:00 -0300

    V3

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const TIRAR = document.getElementById('tirar');
 const pop = new Audio('sound/pop2.wav');
 const win = new Audio('sound/victoria.mp3');
+const gameover= new Audio('sound/gameover.wav')
 var emogis = ['😼','😾','😿','🙀','😽','😻','😹','😸','😺'];
 let vueltas = 10;
 const C1= document.getElementById('casilla1');
 const C2= document.getElementById('casilla2');
 const C3= document.getElementById('casilla3');
+const SUMAR = document.getElementById('sumar');
+SUMAR.disabled=true;
+let vidas = 9;
+const VREST= document.getElementById('vidasrestantes');
+const GATO =document.getElementById('gato');
+const REINTENTAR = document.getElementById('reintentar');
+REINTENTAR.disabled=true;
 
 
+REINTENTAR.addEventListener('click',()=>{location.reload()})
 
 
 
@@ -15,6 +24,18 @@ const C3= document.getElementById('casilla3');
 TIRAR.addEventListener('click',jugar)
 
 function jugar(){
+    
+
+    vidas = vidas-1;
+    
+
+    //Game Over
+    if(vidas<=0){TIRAR.disabled=true;gameover.play();
+    VREST.style.color='red';
+        GATO.style.backgroundColor='rgba(255,0,0,0.2)';
+        SUMAR.disabled=true;
+        REINTENTAR.disabled=false;
+    }
     pop.play();
 
 let combinacion=[];
@@ -22,17 +43,25 @@ let combinacion=[];
 for(let i=0;i<3;i++){
     combinacion.push(emogis[Math.floor(Math.random()*emogis.length)])}
 
+    if(combinacion[0]==combinacion[1]||combinacion[0]==combinacion[2]){
+        SUMAR.disabled= false;
+        
+    }else {SUMAR.disabled=true;}
+
 C1.textContent=combinacion[0]
 C2.textContent=combinacion[1]
 C3.textContent=combinacion[2]
 
+
+
 if(combinacion[0]==combinacion[1]&&combinacion[0]==combinacion[2]){
-    TIRAR.disabled=true;
+    TIRAR.disabled=true;SUMAR.disabled=true;
 win.play();
-}
-
-
-
+GATO.style.backgroundColor='rgba(0,255,0,0.2)';
+REINTENTAR.disabled=false;
 
 
 }
+VREST.textContent="VIDAS: "+vidas;
+}
+SUMAR.addEventListener('click',()=>{vidas = vidas+1;VREST.textContent="VIDAS: "+vidas;SUMAR.disabled=true;})
